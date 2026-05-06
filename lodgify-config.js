392126f40a9c05d56827bae9c8affda905cc0523
rcexclusif-beep <rcexclusif@gmail.com>
2026-05-06 19:39:54 +0200

    test

--- lodgify-config.js
+++ lodgify-config.js
@@ -5,37 +5,37 @@ const LODGIFY_RENTALS = [
   {
     id: '797562',
     name: 'Gîte Pecharmant',
-    slug: 'gite-pecharmant'
+    slug: 'gite-pecharmant-6-pers-chateau-piscine-spa'
   },
   {
     id: '797563',
     name: 'Gîte Montravel',
-    slug: 'gite-montravel'
+    slug: 'gite-montravel-2-pers-chateau-piscine-spa'
   },
   {
     id: '797564',
     name: 'Suite Sauternes',
-    slug: 'suite-sauternes'
+    slug: 'suite-sauternes-chateau-piscine-et-spa'
   },
   {
     id: '797565',
     name: 'Double Suite St Émilion',
-    slug: 'suite-st-emilion'
+    slug: 'double-suite-st-emilion-4-pers-spa-piscine'
   },
   {
     id: '797566',
     name: 'Suite Puygrenier',
-    slug: 'suite-puygrenier'
+    slug: 'suite-puygrenier-4-pers-chateau-piscine-spa'
   },
   {
     id: '797567',
     name: 'Suite Puisseguin',
-    slug: 'suite-puisseguin'
+    slug: 'suite-puisseguin-chateau-spa-piscine'
   },
   {
     id: '797568',
     name: 'Suite Pomerol',
-    slug: 'suite-pomerol'
+    slug: 'suite-pomerol-chateau-puygrenier-spa-piscine'
   }
 ];
 
